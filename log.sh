3e2f70f4ff2f0614ed0171f444359210b2535b6d
azizatoev <a.atoev93@gmail.com>
2025-09-14 05:37:16 +0500

    Initial Commit

--- log.sh
+++ log.sh
@@ -7,30 +7,33 @@ VERBOSE="${VERBOSE:-1}"
 COLOR="${COLOR:-1}"
 
 function info() {
+  local message="$1"
   if [[ "$VERBOSE" -eq 0 ]]; then
     return
   fi
   if [[ "$COLOR" -eq 1 ]]; then
-    ansi_span "\033[0;36m$(timestamp) [INFO]" " $1\n">&2
+    ansi_span "$(timestamp) " "\033[0;36m[INFO] $message\n" >&2
   else
-    printf "[%s] [INFO] %s\n" "$(timestamp)" "$1" >&2
+    printf "[%s] [INFO] %s\n" "$(timestamp)" "$message" >&2
   fi
 }
 
 function warn() {
+  local message="$1"
   if [[ "$COLOR" -eq 1 ]]; then
-    ansi_span "\033[0;33m$(timestamp) [WARN]" " $1\n" >&2
+    ansi_span "$(timestamp) " "\033[0;33m[WARN] $message\n" >&2
   else
-    printf "[%s] [WARN] %s\n" "$(timestamp)" "$1" >&2
+    printf "[%s] [WARN] %s\n" "$(timestamp)" "$message" >&2
   fi
 }
 
 function error() {
+  local message="$1"
   local code="${2:-1}"
   if [[ "$COLOR" -eq 1 ]]; then
-    ansi_span "\033[0;31m$(timestamp) [ERROR]" " $1 (exit code: $code)\n">&2
+    ansi_span "$(timestamp) " "\033[0;31m[ERROR] $message (exit code: $code)\n" >&2
   else
-    printf "[%s] [ERROR] %s (exit code: %d)\n" "$(timestamp)" "$1" "$code" >&2
+    printf "[%s] [ERROR] %s (exit code: %d)\n" "$(timestamp)" "$message" "$code" >&2
   fi
   exit "$code"
 }
