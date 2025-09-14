8da5f32f5085881544bd062ddab0d9c8dd2b0559
azizatoev <a.atoev93@gmail.com>
2025-09-14 21:02:21 +0500

    Initial Commit

--- ansi.sh
+++ ansi.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+ANSI_ESC=$'\033'
+ANSI_PATTERN="$ANSI_ESC\[[0-9;?]*[A-Za-z]"
+
 function ansi_span() {
   local arg
 
   for arg in "$@"; do
-    printf "%b\033[0m" "$arg"
+    if [[ "$arg" =~ $ANSI_PATTERN ]]; then
+      # append reset only if color code is present
+      printf "%b%s" "$arg" "${ANSI_ESC}[0m"
+    else
+      printf "%b" "$arg"
+    fi
   done
 }
